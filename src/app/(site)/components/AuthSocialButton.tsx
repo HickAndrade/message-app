da157a7804e9fc55ff92ec041dff42c401533936
HickAndrade <henrique_andrade2013@hotmail.com>
2023-08-02 21:20:49 -0300

    feat:auth w/ next-auth x prisma

---
+++ src/app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { IconType } from "react-icons";
+
+interface AutoSocialButtonProps {
+    icon: IconType;
+    onClick: ()=> void;
+}
+
+export const AuthSocialButton = ({ icon: Icon, onClick }: AutoSocialButtonProps) => {
+  return (
+    <button
+    type="button"
+    onClick={onClick}
+    className="
+    inline-flex 
+    w-full
+    justify-center
+    rounded-md
+    bg-white
+    px-4 
+    py-2
+    text-gray-500
+    shadow-sm 
+    ring-1
+    ring-inset
+    ring-gray-300
+    hover:bg-gray-50
+    focus:outline-offset-0
+    "
+    ><Icon /></button>
+  )
+}
